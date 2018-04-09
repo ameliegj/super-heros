fd74412cf144c69efcc06a8b58ba82d5907ee6f3
ameliegj <ameliegrosjean@gmail.com>
2018-04-09 11:21:42 +0200

    set up dev env

---
+++ dist/index.php
@@ -0,0 +1,31 @@
+<?php
+    // Config
+    include 'config.php';
+    
+    // Routing
+
+    if (empty( $_GET['q']))
+    {
+        $q = '';
+    } 
+    else 
+    {
+        $q = $_GET['q'];
+    }
+
+    if($q === '')
+    {
+        $page = 'home';
+    }
+    else
+    {
+        $page = '404';
+    }
+    
+    // Includes
+    // include 'views/partials/header.php';
+    include 'views/pages/'.$page.'.php';
+    // include 'views/partials/footer.php';
+
+
+?>
