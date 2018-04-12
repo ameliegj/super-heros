b56e2033703e84fd87ba1f106bdcd7e8cb27805f
Kimeld <amed.doukoure@hetic.net>
2018-04-12 17:48:01 +0200

    movie creation

---
+++ dist/views/pages/movie.php
@@ -0,0 +1,81 @@
+<div class="ratingContainer">
+    <h1 class="col-lg-offset-2">MOVIE RATING</h1>
+    <h2 class="col-lg-offset-1">Phase 1 - 2008/2012</h2>
+
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">Iron Man</div>
+        <div class="lacetRed col-lg-offset-2">100 000$</div>
+        <div class="bubbleRed col-lg-offset-8"></div>
+
+    </div>
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">The terrible Hulk</div>
+        <div class="lacetGreen col-lg-offset-2">100 000$</div>
+        <div class="bubbleGreen col-lg-offset-5"></div>
+
+    </div>
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">Iron Man 2</div>
+        <div class="lacetRed-2 col-lg-offset-2">100 000$</div>
+        <div class="bubble col-lg-offset-6"></div>
+
+    </div>
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">Thor</div>
+        <div class="lacetRed-3 col-lg-offset-2">100 000$</div>
+        <div class="bubble col-lg-offset-7"></div>
+
+    </div>
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">Captain america: The first Avengers</div>
+        <div class="lacetBlue col-lg-offset-2">100 000$</div>
+        <div class="bubbleBlue col-lg-offset-7"></div>
+
+    </div>
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">Avengers</div>
+        <div class="lacetRed col-lg-offset-2">100 000$</div>
+        <div class="bubbleBlue col-lg-offset-8"></div>
+
+    </div>
+
+
+
+    <h2 class="col-lg-offset-1">Phase 2 - 20013/2015</h2>
+
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">Iron Man 3</div>
+        <div class="lacetRed col-lg-offset-2">100 000$</div>
+        <div class="bubbleRed col-lg-offset-8"></div>
+
+    </div>
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">Thor: the dark world</div>
+        <div class="lacetGreen col-lg-offset-2">100 000$</div>
+        <div class="bubbleGreen col-lg-offset-5"></div>
+
+    </div>
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">Captain America: the Winter soldier</div>
+        <div class="lacetBlue col-lg-offset-2">100 000$</div>
+        <div class="bubbleBlue col-lg-offset-7"></div>
+
+    </div>
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">Guardian of the galaxie</div>
+        <div class="lacetBlue-2 col-lg-offset-2">100 000$</div>
+        <div class="bubblePurple col-lg-offset-8"></div>
+
+    </div>
+    <div class="rating col-lg-offset-2">
+        <div class="col-lg-offset-1">Avengers: age of ultron</div>
+        <div class="lacetRed-4 col-lg-offset-2">100 000$</div>
+        <div class="bubbleRed col-lg-offset-7"></div>
+
+    </div>
+
+
+
+
+
+</div>
